b09f5f5c2789c28cce96a0068aecc7ea2af43f7c
oj9040 <oj9040@gmail.com>
2019-05-24 11:40:38 -0400

    shell modified

--- example_supervised.sh
+++ example_supervised.sh
@@ -1,11 +1,11 @@
 
 # to reproduce the result in Table 3 in paper 
 # ppi
-python -m graphsage.supervised_train --train_prefix ~/data/ppi/ppi --model mean_add --dim_1 512 --dim_2 512 --samples_1 25 --samples_2 10 --batch_size 512 --validate_batch_size 512 --gpu 0 --sigmoid --epochs 100
+python -m graphsage.supervised_train --train_prefix ~/data/ppi/ppi --model mean_add --dim_1 512 --dim_2 512 --samples_1 25 --samples_2 10 --batch_size 32 --validate_batch_size 32 --gpu 0 --sigmoid --epochs 100
 
 # reddit
 python -m graphsage.supervised_train --train_prefix ~/data/reddit/reddit --model mean_add --dim_1 512 --dim_2 512 --samples_1 25 --samples_2 10 --batch_size 32 --validate_batch_size 32 --gpu 0 --epochs 100
 
 # pubmed
-python -m graphsage.supervised_train --train_prefix ~/data/pubmed/pubmed --model mean_add --dim_1 512 --dim_2 512 --samples_1 25 --samples_2 10 --batch_size 512 --validate_batch_size 512 --gpu 0 --epochs 100
+python -m graphsage.supervised_train --train_prefix ~/data/pubmed/pubmed --model mean_add --dim_1 512 --dim_2 512 --samples_1 25 --samples_2 10 --batch_size 32 --validate_batch_size 32 --gpu 0 --epochs 100
 
